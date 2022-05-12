b24d120a8c74b4b34b5971f421841ed5dc524e90
slabcut <dave@holycow.com.au>
2022-05-12 14:23:13 +1000

    1 file modified

--- src/App.js
+++ src/App.js
@@ -6,9 +6,8 @@ export default function App() {
     <div>
       <h1>Hello StackBlitz! this is a test try committing</h1>
       <h2>1 2 3 4 5 6 7 8 9 0</h2>
+      <h2>1 2 3 4 5 6 7 8 9 0</h2>
       <p>Start editing to see some magic yayay yip :)</p>
-
-      
     </div>
   );
 }

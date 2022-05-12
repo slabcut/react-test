5d457410291eb800e5c0518b6893cc5473720335
slabcut <dave@holycow.com.au>
2022-05-12 14:17:48 +1000

    1 file modified

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import "./style.css";
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz! this is a test try committin</h1>
+      <h1>Hello StackBlitz! this is a test try committing</h1>     
+      
+       <h2>1 2 3 4 5 6 7 8 9 0</h2>
+
       <p>Start editing to see some magic yayay yip :)</p>
     </div>
   );
